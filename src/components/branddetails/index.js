85b8f5c9b6caddb19f7f2fe2c26cf94170ee2027
Phu Le <phule9225@gmail.com>
2021-09-27 20:25:41 +0700

    not done

--- src/components/branddetails/index.js
+++ src/components/branddetails/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Link, useParams } from "react-router-dom"
 import { FormattedMessage } from 'react-intl';
 import { useBrandDetails } from '../../talons/useBrandDetails';
+import { usePagination } from '@magento/peregrine';
 import defaultClasses from './branddetails.css';
 
-import { fullPageLoadingIndicator } from '@magento/venia-ui/lib/components/LoadingIndicator';
-import { Title, Meta } from '@magento/venia-ui/lib/components/Head';
+// import { fullPageLoadingIndicator } from '@magento/venia-ui/lib/components/LoadingIndicator';
+// import { Title, Meta } from '@magento/venia-ui/lib/components/Head';
 import Products from './products';
+import myStyles from "./branddetails.css";
 
 
 function BrandDetail() {
@@ -14,6 +16,35 @@ function BrandDetail() {
 	const { brandUrl = "" } = useParams();
 	const { brandData, brandLoading, derivedErrorMessage } = useBrandDetails({ url_key: brandUrl.replace('.html', '') });
 	console.log("brandData: ", brandData)
+
+	// const [paginationValues, paginationApi] = usePagination();
+	// const { currentPage, totalPages } = paginationValues;
+	// const { setCurrentPage, setTotalPages } = paginationApi;
+
+	// const pageControl = {
+	// 	currentPage,
+	// 	setPage: setCurrentPage,
+	// 	totalPages
+	// };
+
+	// console.log("pageControl: ", pageControl)
+
+	// const totalPagesFromData = data
+	// 	? data.products.page_info.total_pages
+	// 	: null;
+
+	// useEffect(() => {
+	// 	setTotalPages(totalPagesFromData);
+	// 	return () => {
+	// 		setTotalPages(null);
+	// 	};
+	// }, [setTotalPages, totalPagesFromData]);
+
+	// useEffect(() => {
+	// 	if (error && !loading && currentPage !== 1) {
+	// 		setCurrentPage(1);
+	// 	}
+	// }, [currentPage, error, loading, setCurrentPage]);
 
 	if (!brandData || !brandData.mpbrand || !brandData.mpbrand.items || !brandData.mpbrand.items[0])
 		return (
@@ -23,12 +54,17 @@ function BrandDetail() {
 		);
 
 	const brandInformation = brandData.mpbrand.items[0];
-	console.log("brandInformation: ", brandInformation)
+	console.log("brandData: ", brandData)
 	return (
 		<div className={classes.rootDetails}>
-			<h1>Hoang Long Details {brandUrl}</h1>
-
-			<Products option_id={brandInformation.option_id}></Products>
+			<h1>{brandInformation.page_title}</h1>
+			<section>
+				<div className={myStyles.gallery_root}>
+					<div className={myStyles.gallery_items}>
+						<Products option_id={brandInformation.option_id}></Products>
+					</div>
+				</div>
+			</section>
 		</div >
 	)
 }
--- src/components/branddetails/products/index.js
+++ src/components/branddetails/products/index.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react'
 import { useLazyQuery, useQuery } from '@apollo/client';
 import GET_BRAND_PRODUCTS from "../../branddetails/getBrandProducts.graphql"
+
+import myStyles from "./style.css";
+
 function Products(props) {
 	const { option_id } = props;
 	const [runQuery, queryResponse] = useLazyQuery(GET_BRAND_PRODUCTS);
@@ -26,13 +29,40 @@ function Products(props) {
 		runQuery,
 	]);
 
-	console.log("data product: ", data)
+	if (data === undefined) {
+		return <> <h1>Loading</h1> </>
+	}
+	const dataList = data === undefined ? {} : data
 
+	console.log("data product 1: ", loading)
+	console.log("data product 2: ", dataList.products.items)
+
+	function renderListProduct() {
+		return dataList.products.items.map((item, index) => {
+			return (
+
+				<div className={myStyles.item_root} key={index}>
+					<div className={myStyles.item_images}>
+						<div className={myStyles.item_imageContainer + " " + myStyles.image_container}>
+							<img className={myStyles.item_image} src={item.small_image.url} alt={item.name} height="375" />
+						</div>
+						<div className={myStyles.item_name}>
+							{item.name}
+						</div>
+						<div className={myStyles.item_price}>
+							{item.price.regularPrice.amount.value + " " + item.price.regularPrice.amount.currency}
+						</div>
+					</div>
+				</div>
+
+			)
+		})
+	}
 
 	return (
-		<div>
-			<h1>Hoang Long</h1>
-		</div>
+		<>
+			{renderListProduct()}
+		</>
 	)
 }
 
